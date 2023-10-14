b9e53c39f3f1f9155ffefd46204cea6a0dc5c79e
aditisethi74 <131951754+aditisethi74@users.noreply.github.com>
2023-10-15 00:13:21 +0530

    first commit

---
+++ src/main/java/com/cool/abc/firstpro/App.java
@@ -0,0 +1,13 @@
+package com.cool.abc.firstpro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
